8b88689c0e23eea624f1006d6a5e90042570ce17
the creature <pc@MacBook-Air-PC.local>
2025-06-16 00:30:26 +0300

    create notifications

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,9 +3,10 @@ import Logo from '../Shared/Logo'
 import { Badge, Avatar } from 'antd'
 import BasketDrawer from '../BasketDrawer'
 import { Link } from 'react-router-dom'
+import { useCart } from '../CartContext'
 
 const Header = () => {
-	const [notification, setNotification] = useState(0)
+	const { items, favorites } = useCart()
 	const [openBasket, setOpenBasket] = useState(false)
 
 	return (
@@ -14,43 +15,41 @@ const Header = () => {
 				<Logo />
 			</Link>
 			<ul className='d-flex'>
-				<button
-					className='btnStyle'
-					onClick={() => setOpenBasket(prev => !prev)}
-				>
-					<img src='/img/cart.svg' alt='Basket' />
-					<span>Корзина</span>
-				</button>
+				<Badge count={items.length} offset={[-120, 0]}>
+					<button
+						className='btnStyle'
+						onClick={() => setOpenBasket(prev => !prev)}
+					>
+						<img src='/img/cart.svg' alt='Basket' />
+						<span>Корзина</span>
+					</button>
+				</Badge>
 
 				<BasketDrawer openBasket={openBasket} setOpenBasket={setOpenBasket} />
-
-				<Link to='/favorites' className='d-flex items-center justify-center'>
-					<button className='btnStyle'>
-						<div className='heart-icon'>
-							<img src='/img/heart.svg' alt='Fav' />
-						</div>
-						<span>Закладки</span>
-					</button>
-				</Link>
+				<Badge dot={favorites.length > 0} offset={[-130, 0]}>
+					<Link to='/favorites' className='d-flex items-center justify-center'>
+						<button className='btnStyle'>
+							<div className='heart-icon'>
+								<img src='/img/heart.svg' alt='Fav' />
+							</div>
+							<span>Закладки</span>
+						</button>
+					</Link>
+				</Badge>
 				<li className='d-flex align-center mr-30'>
 					<Link to='/account'>
-						<Badge count={notification}>
-							<button
-								onClick={() => setNotification(notification + 1)}
-								style={{
-									direction: 'none',
-									border: 'none',
-									background: 'transparent',
-									cursor: 'pointer',
-								}}
-							>
-								<Avatar
-									style={{ backgroundColor: '#fde3cf', color: '#f56a00' }}
-								>
-									U
-								</Avatar>
-							</button>
-						</Badge>
+						<button
+							style={{
+								direction: 'none',
+								border: 'none',
+								background: 'transparent',
+								cursor: 'pointer',
+							}}
+						>
+							<Avatar style={{ backgroundColor: '#fde3cf', color: '#f56a00' }}>
+								U
+							</Avatar>
+						</button>
 					</Link>
 				</li>
 			</ul>
